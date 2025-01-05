08b3e180f5ecac088aede0d9aac856a09ff441f0
idmaksim <dmaksim@tuta.io>
2025-01-05 15:27:43 +0300

    Add environment variables for S3 and Redis configuration
    - Updated config.ts to include new environment variables for S3 integration: S3_ACCESS_KEY, S3_SECRET_KEY, S3_ENDPOINT, S3_REGION, and S3_BUCKET.
    - Added REDIS_URL to support Redis configuration.
    
    This update enhances the application's configuration capabilities for cloud storage and caching services.

--- apps/main/src/config/config.ts
+++ apps/main/src/config/config.ts
@@ -3,4 +3,10 @@ export default () => ({
   DATABASE_URL: process.env.DATABASE_URL,
   ACCESS_SECRET: process.env.ACCESS_SECRET,
   REFRESH_SECRET: process.env.REFRESH_SECRET,
+  REDIS_URL: process.env.REDIS_URL,
+  S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+  S3_SECRET_KEY: process.env.S3_SECRET_KEY,
+  S3_ENDPOINT: process.env.S3_ENDPOINT,
+  S3_REGION: process.env.S3_REGION,
+  S3_BUCKET: process.env.S3_BUCKET,
 });
